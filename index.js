9997420a998d427afed1cbc56c1163ba93fe3f86
Sconzo <rspolydoro@gmail.com>
2021-11-23 07:39:28 -0300

    bug fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello World aaa!');
+   res.send('Hello World!');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
